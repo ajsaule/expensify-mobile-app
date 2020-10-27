6c5fc55be9a12d04b25d6e706f4d8b25e44e2880
andrej <andrejsaule8@gmail.com>
2020-10-27 11:47:02 +1100

    latest chagnes

--- components/Tracker.js
+++ components/Tracker.js
@@ -4,24 +4,26 @@ import { View, Text, Image, StyleSheet, Button, TextInput, FlatList } from 'reac
 
 export default function Events({ history }) {
 
-    const [expense, setExpense] = useState(null)
+    const [amount, setAmount] = useState(null)
+    const [type, setType] = useState(null)
     const [expenseArr, setExpenseArr] = useState([])
     const [list, setList] = useState(null)
     
-    const handleSubmit = (e) => {
-        let { name, value } = e // how do I reset the text field without using inner HTML ?? 
-        expenseArr.push(expense)
-        console.log(expenseArr)
+    const handleSubmit = () => {
+        setExpenseArr({...expenseArr, "amount": amount, "type": type })
+        // let { name, value } = e // how do I reset the text field without using inner HTML ?? 
+        // expenseArr.push(expense)
+        // console.log(expenseArr)
         printItems()
     }
     
-    const handleChange = (e, stateObject) => {
-        console.log(e)
-        const { name, value } = e 
-        console.log(name)
-        console.log(value)
-        setExpense({ ...stateObject, [name]: value })
-    }
+    // const handleChange = (e, stateObject) => {
+    //     console.log(e)
+    //     const { name, value } = e 
+    //     console.log(name)
+    //     console.log(value)
+    //     setExpense({ ...stateObject, [name]: value })
+    // }
 
     const printItems = () => {
         // how can we simplify the loop here ? 
@@ -45,9 +47,9 @@ export default function Events({ history }) {
             <Button title="Back" onPress={() => history.push('/')}></Button>
             <Text>Calculate your expenses</Text>
             <Text>How much you spent</Text>
-            <TextInput onChangeText={(e) => handleChange(e, expense)} name="amount" type="number" />
+            <TextInput onChangeText={(number) => setAmount(number)} name="amount" type="number" />
             <Text>What did you spend it on</Text>
-            <TextInput onChangeText={(e) => handleChange(e, expense)} name="type" type="text" />
+            <TextInput onChangeText={(expense) => setType(expense)} name="type" type="text" />
             <Button onPress={handleSubmit} type="submit" title="Log"/>
             <Text>Expenses</Text>
             <Text>
